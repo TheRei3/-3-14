e86290e80f14926653fe031023729904f95b216d
admin <admin@DESKTOP-8L2IG92>
2021-04-21 05:33:36 +0900

    ex3no14

---
+++ ex3no14/src/ex3no14/ex3no14.java
@@ -0,0 +1,26 @@
+package ex3no14;
+
+public class ex3no14 { //104p
+	public static void main(String[] args) {
+		char c = 'a';
+		
+		for(int i = 0; i < 26 ; i++) {
+			System.out.print(c++);
+		}
+		System.out.println();
+		
+		c = 'A';
+		for(int i = 0; i < 26 ; i++) {
+			System.out.print(c++);
+		}
+		System.out.println();
+		
+		c = '0';
+		for(int i = 0; i < 10 ; i++) {
+			System.out.print(c++);
+		}
+		System.out.println();
+		
+	}
+   
+}
